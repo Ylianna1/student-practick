2cce0afeef82a5a766fc6638165b601f2c727506
Ylianna1 <95632183+Ylianna1@users.noreply.github.com>
2023-01-27 15:33:23 +0300

    Add files via upload

---
+++ src/components/StudentInfo.vue
@@ -0,0 +1,32 @@
+<template>
+   {{student.name}}
+   Робота {{isDonePrOfStudent}}
+</template>
+
+<script>
+import axios from 'axios'
+  
+export default {
+    props: {
+        id: '',
+    },
+    data(){
+        return{
+            student
+        };
+    },
+    mounted: function(){
+         axios.get("http://34.82.81.113:3000/students"+this.id).then((response) =>{
+             this.student = response.data;
+         }) ;
+    },
+    computed: {
+    isDonePrOfStudent() {
+        return this.student.isDonePr ? 'здана' : 'не здана'
+    }
+    }
+}    
+</script>
+
+<style scoped>
+</style>
